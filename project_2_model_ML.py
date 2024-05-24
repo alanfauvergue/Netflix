7757d75fb5981424f7ab03ee099be51b7abb4814
alanfauvergue <168745000+alanfauvergue@users.noreply.github.com>
2024-05-24 21:55:01 +0200

    Update project_2_model_ML.py

--- project_2_model_ML.py
+++ project_2_model_ML.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[84]:
+# In[1]:
 
 
 import pandas as pd
@@ -9,105 +9,118 @@ import numpy as np
 from sklearn.neighbors import NearestNeighbors
 
 
-# In[85]:
+# In[67]:
 
 
 df_fr = pd.read_csv("df_fr_final2405.csv")
 df_fr.head()
 
 
-# In[86]:
+# In[69]:
+
+
+df_fr.iloc[:,10:] = df_fr.iloc[:,10:].apply(lambda x : x*2)
+
+
+# In[72]:
 
 
 df_fr["release_date"] = pd.to_datetime(df_fr["release_date"])
 df_fr["release_date"] = df_fr["release_date"].apply(lambda x : x.year)
 
 
-# In[104]:
+# In[73]:
 
 
 var_exp = df_fr.loc[:,"averageRating"::].columns
 var_y = "originalTitle"
 
 
-# In[105]:
+# In[74]:
 
 
 X = df_fr[var_exp]
 y = df_fr[var_y]
 
 
-# In[134]:
+# In[75]:
 
 
 var_y = "Intouchables"
 
 
-# In[135]:
+# In[76]:
 
 
 df_fr_nneighbors = df_fr[df_fr["originalTitle"] != var_y]
 df_fr_y = df_fr[df_fr["originalTitle"] == var_y]
 
 
-# In[136]:
+# In[77]:
 
 
 df_fr_nneighbors[df_fr["originalTitle"] == var_y]
 
 
-# In[137]:
+# In[78]:
 
 
-X_train = df_fr[var_exp]
+X_train = df_fr_nneighbors[var_exp]
 
 
-# In[138]:
+# In[79]:
 
 
 df_fr_y
 
 
-# In[139]:
+# In[80]:
 
 
 modelNN = NearestNeighbors(n_neighbors = 4)
 modelNN.fit(X_train)
 
 
-# In[127]:
+# In[81]:
 
 
 X_train
 
 
-# In[140]:
+# In[82]:
 
 
 df_fr_y.iloc[:,2:]
 
 
-# In[147]:
+# In[83]:
 
 
 df_fr[290:291]
 
 
-# In[148]:
+# In[84]:
 
 
 df_fr[468:469]
 
 
-# In[142]:
+# In[85]:
 
 
 neighbors = modelNN.kneighbors(df_fr_y.iloc[:,2:])
-neighbors
+proposition_film = list(neighbors[1][0][0:])
 
 
-# In[ ]:
+# In[86]:
 
 
+proposition_film
 
+
+# In[54]:
+
+
+for film in proposition_film:
+    print(df_fr.loc[film,"originalTitle"])
 
